5ce40b3d84da1c2de9bb717f92cabcd614e54cf6
Eric D. Schabell <eric@schabell.org>
2011-12-21 13:40:38 +0100

    Initial project import, a jBPM 5.2 final based example that uses a test class to run a decision based on a rule node.

---
+++ src/main/java/com/sample/ProcessTest.java
@@ -0,0 +1,64 @@
+package com.sample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.drools.KnowledgeBase;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+//import org.drools.runtime.rule.FactHandle;
+import org.drools.runtime.process.WorkflowProcessInstance;
+// import org.jbpm.examples.request.Person;
+// import org.jbpm.examples.request.Request;
+import org.jbpm.process.workitem.wsht.WSHumanTaskHandler;
+
+/**
+ * This is a sample file to launch a process.
+ */
+public class ProcessTest {
+
+	public static final void main(String[] args) {
+		try {
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "test", 1000);
+			ksession.getWorkItemManager().registerWorkItemHandler("Human Task", new WSHumanTaskHandler());
+			// start a new process instance
+			
+			Person person = new Person("john", "John Doe");
+			person.setAge(16);
+			Request request = new Request("12345");
+			request.setPersonId("john");
+			request.setAmount(1000L);
+			ksession.insert(person);
+//			FactHandle handle = ksession.insert(request);
+
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("aPerson", person);
+			params.put("aRequest", request);
+			WorkflowProcessInstance processInstance = (WorkflowProcessInstance) ksession.startProcess("com.sample.bpmn.hello", params);
+			ksession.insert(processInstance);
+			ksession.fireAllRules();
+			System.out.println("Rules Fired ...");
+			logger.close();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource("sample.bpmn"), ResourceType.BPMN2);
+		kbuilder.add(ResourceFactory.newClassPathResource("validation.drl"),
+				ResourceType.DRL);
+
+		return kbuilder.newKnowledgeBase();
+	}
+
+}
